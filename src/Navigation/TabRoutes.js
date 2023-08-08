3924dc451ae09489ed4446bbe3fa0c37e8edfa76
suryabhansing <117063440+suryabhansing@users.noreply.github.com>
2023-08-08 22:09:16 +0530

    tab style

--- src/Navigation/TabRoutes.js
+++ src/Navigation/TabRoutes.js
@@ -5,25 +5,63 @@ import {HomeScreen, ProfileScreen, ExploreScreen} from '../Screens/index';
 import navigationStrings from '../constants/navigationStrings';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Image} from 'react-native';
-
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 const Tab = createBottomTabNavigator();
 
 function TabRoutes() {
   return (
     <Tab.Navigator
       initialRouteName={navigationStrings.HOME}
-      screenOptions={{headerShown: false}}>
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: 'red',
+        tabBarInactiveTintColor: 'gray',
+      }}>
       <Tab.Screen
         name={navigationStrings.HOME}
         component={HomeScreen}
         options={{
           tabBarIcon: ({focused}) => {
-            return <Image />;
+            return (
+              <MaterialCommunityIcons
+                name="home"
+                size={30}
+                color={focused ? 'red' : null}
+              />
+            );
           },
         }}
       />
-      <Tab.Screen name={navigationStrings.PROFILE} component={ProfileScreen} />
-      <Tab.Screen name={navigationStrings.EXPLORE} component={ExploreScreen} />
+      <Tab.Screen
+        name={navigationStrings.PROFILE}
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return (
+              <MaterialCommunityIcons
+                name="account-circle"
+                size={30}
+                color={focused ? 'red' : null}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name={navigationStrings.EXPLORE}
+        component={ExploreScreen}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return (
+              <MaterialCommunityIcons
+                name="shield-search"
+                size={30}
+                color={focused ? 'red' : null}
+              />
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 }
